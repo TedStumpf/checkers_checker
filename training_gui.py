a47ea02544387aad5d13a1c03d77a084c4bf31ae
Unknown <theodorestumpf@gmail.com>
2018-12-14 10:32:46 -0500

    Added training_gui.py

---
+++ training_gui.py
@@ -0,0 +1,8 @@
+#   training_gui.py
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
