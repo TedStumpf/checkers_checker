b87d598bfe67b7c6199bfa041c3030cca610c0dc
Theodore Stumpf <theodorestumpf@gmail.com>
2019-01-09 09:47:51 -0500

    Added more functions to overseer.py
    get_tournament_status
    set_up_tournament
    do_tournament_step
    is_tournament_completed
    get_pop_size
    get_games_played

--- overseer.py
+++ overseer.py
@@ -24,3 +24,51 @@ class Overseer():
     def create_starting_population(self):
         for i in range(self.population_size):
             self.population.append(LinearAnalyzer(True))
+
+    def get_tournament_status(self):
+        return (self.tournament_player, self.tournament_game)
+
+
+    def set_up_tournament(self):
+        #   Create a list of player IDs
+        player_ids = [[i] for i in range(self.population_size)]
+        #   Fill out each list
+        for brk in player_ids:
+            while (len(brk) <= self.games_played):
+                n = randrange(self.population_size)
+                if (not n in brk):
+                    brk.append(n)
+
+        self.tournament = player_ids
+        self.tournament_player = 0
+        self.tournament_game = 0
+
+    def do_tournament_step(self):
+        if (not self.is_tournament_completed()):
+            print("Player ", self.tournament[self.tournament_player][0], " vs Player ", 
+                    self.tournament[self.tournament_player][self.tournament_game + 1], sep = "", end = ":  ")
+
+            board = GameBoard()
+            winner = board.play_game(self.population[self.tournament[self.tournament_player][0]], 
+                    self.population[self.tournament[self.tournament_player][self.tournament_game + 1]])
+            if (winner == 0):
+                print("Draw")
+            elif (winner == 1):
+                print("Won")
+            else:
+                print("Lost")
+            self.tournament_game += 1
+            if (self.tournament_game == self.games_played):
+                self.tournament_player += 1
+                self.tournament_game = 0
+                print()
+
+
+    def is_tournament_completed(self):
+        return (self.tournament_player == self.population_size)
+
+    def get_pop_size(self):
+        return self.pop_size
+
+    def get_games_played(self):
+        return self.games_played
