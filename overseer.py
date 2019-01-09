788ec81de2781f720462fdebadbc2b0e0d99fa07
Theodore Stumpf <theodorestumpf@gmail.com>
2019-01-09 09:29:41 -0500

    Converted overseer tabs to spaces

--- overseer.py
+++ overseer.py
@@ -1,23 +1,26 @@
 #   overseer.py
 #   Theodore Stumpf
-#	Manages tournaments between contestants
+#   Manages tournaments between contestants
 from lin_analyzer import LinearAnalyzer
-from random import shuffle
+from board import GameBoard
+from random import shuffle, randrange
 
 class Overseer():
 
-	def __init__(self, pop_size = 500, batches = 1, top_keep = 0.05, mix_keep = 0.4, new_pop = 0.1, games = 5):
-		self.population_size = pop_size
-		self.batches = batches
-		self.top_keep_perc = top_keep
-		self.mix_keep_perc = mix_keep
-		self.new_pop_perc = new_pop
-		self.games_played = games
+    def __init__(self, pop_size = 500, batches = 1, top_keep = 0.05, mix_keep = 0.4, new_pop = 0.1, games = 5):
+        self.population_size = pop_size
+        self.batches = batches
+        self.top_keep_perc = top_keep
+        self.mix_keep_perc = mix_keep
+        self.new_pop_perc = new_pop
+        self.games_played = games
 
-		self.generation = 1
-		self.tournament_status = 0
-		self.population = []
+        self.generation = 1
+        self.tournament_player = 0
+        self.tournament_game = 0
+        self.population = []
+        self.tournament = []
 
-	def create_starting_population(self):
-		for i in range(self.population_size):
-			self.population.append(LinearAnalyzer(True))
+    def create_starting_population(self):
+        for i in range(self.population_size):
+            self.population.append(LinearAnalyzer(True))
